e4949a29a8af196744f9773ada49353c238f41f4
ethan <programcccethan@gmail.com>
2018-07-02 16:11:38 +0800

    Initial commit

---
+++ src/main/java/com/dubbodemo/test/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.dubbodemo.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liwenqing on 2018/6/26.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "hello Spring MVC";
+    }
+
+
+}
---
+++ src/main/java/com/dubbodemo/test/dubbo/IDubboService.java
@@ -0,0 +1,10 @@
+package com.dubbodemo.test.dubbo;
+
+/**
+ * Created by liwenqing on 2018/7/2.
+ */
+public interface IDubboService {
+
+    String getString();
+
+}
